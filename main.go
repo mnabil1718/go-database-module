6f76964fffbe2d734c53249cd1c80fde4593e1ad
tingkatin <muhammad.nabil@superindo.co.id>
2024-05-13 11:02:12 +0700

    Added connection function abstraction

--- main.go
+++ main.go
@@ -3,20 +3,33 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func main() {
+func GetConnection() *sql.DB {
 	// connection string format: postgres://username:password@localhost:5432/database_name
 	db, err := sql.Open("pgx", "postgres://postgres:Cucibaju123@localhost:5432/test")
 	if err != nil {
 		panic(err)
 	}
+
+	db.SetMaxIdleConns(10)
+	db.SetMaxOpenConns(100)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+	db.SetConnMaxLifetime(1 * time.Hour)
+
+	return db
+
+}
+
+func main() {
+	db := GetConnection()
 	defer db.Close()
 
 	var greeting string
-	err = db.QueryRow("select 'hello world'").Scan(&greeting)
+	err := db.QueryRow("select 'hello world'").Scan(&greeting)
 	if err != nil {
 		panic(err)
 	}
